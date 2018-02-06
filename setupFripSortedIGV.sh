7cff201c9156c28e22338576426c508a13ece91c
jlchang <jlchang@broadinstitute.org>
2018-02-06 09:25:47 -0500

    fix setup script to reflect version info in housekeeping file to save files in correct dir

--- setupFripSortedIGV.sh
+++ setupFripSortedIGV.sh
@@ -20,6 +20,7 @@ suffix=$(basename $orig)
 type=$(basename $(dirname $orig))
 ssf=$(basename $(dirname $(dirname $orig)))
 expt=${ssf}_${type}_${suffix}
+expt_path=${ssf}/${type}/${suffix}
 
 if [ ! -f sampleByFRiP.txt ]
   then
@@ -27,39 +28,41 @@ if [ ! -f sampleByFRiP.txt ]
     exit
 fi
 
-if [ -f housekeeping_${expt}.txt ]
+if [ -f ${expt}_IGV/housekeeping_${expt}.txt ]
   then
     echo "output file already exists, try again after you:"
-    echo "rm housekeeping_${expt}.txt"
-      if [ -d ${ssf}_${type}_IGV ]
+    echo "rm ${expt}_IGV/housekeeping_${expt}.txt"
+      if [ -d ${expt}_IGV ]
         then
           echo "output dir also exists:"
-          echo "rmdir ${ssf}_IGV"
+          echo "rmdir ${expt}_IGV"
       fi
     exit
 fi
 
-if [ -d ${ssf}_${type}_IGV ]
+if [ -d ${expt}_IGV ]
   then
     echo "output dir exists:"
-    echo "rmdir ${ssf}_IGV"
+    echo "rmdir ${expt}_IGV"
   exit
 fi
 
-mkdir ${orig}/${ssf}_IGV
+mkdir ${orig}/${expt}_IGV
 
-head -n 4 ${SCRIPTDIR}/housekeeping_template.txt | sed "s/SSF-XXXXX/$ssf/g" > ${ssf}_IGV/housekeeping_${expt}.txt
+#echo "making housekeeping file"
+
+head -n 4 ${SCRIPTDIR}/housekeeping_template.txt | sed "s;EXPTPATH;$expt_path;g" | sed "s/SSF-XXXXX/$expt/g" > ${expt}_IGV/housekeeping_${expt}.txt
 
 for i in $(cat ${orig}/sampleByFRiP.txt | cut -f 1)
 do
   bam=$(ls ${orig}/${i}*/${i}*.mapq1.PE.nodup.bam)
   echo "load $bam"
-done >> ${ssf}_IGV/housekeeping_${expt}.txt
+done >> ${expt}_IGV/housekeeping_${expt}.txt
 
-tail -n +5 ${SCRIPTDIR}/housekeeping_template.txt | sed "s/SSF-XXXXX/$expt/g" >> ${ssf}_IGV/housekeeping_${expt}.txt
+tail -n +5 ${SCRIPTDIR}/housekeeping_template.txt | sed "s/SSF-XXXXX/$expt/g" >> ${expt}_IGV/housekeeping_${expt}.txt
 
 echo "To generate IGV snapshots (run on lager):"
 echo "use Java-1.8"
 #all the stuff in igv.sh
 #echo "xvfb-run java -Xmx4000m -XX:+IgnoreUnrecognizedVMOptions --illegal-access=permit --add-modules=java.xml.bind -Dapple.laf.useScreenMenuBar=true -Djava.net.preferIPv4Stack=true -jar /cil/shed/apps/external/IGV/IGV_2.4.6/igv.jar -b ${orig}/${ssf}_IGV/housekeeping_${expt}.txt"
-echo "xvfb-run java -Xmx4000m -jar /cil/shed/apps/external/IGV/IGV_2.4.6/igv.jar -b ${orig}/${ssf}_IGV/housekeeping_${expt}.txt"
+echo "xvfb-run java -Xmx4000m -jar /cil/shed/apps/external/IGV/IGV_2.4.6/igv.jar -b ${orig}/${expt}_IGV/housekeeping_${expt}.txt"
