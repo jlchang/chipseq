820eaa9e86aab1771c7e0a20b07ee3a76116e70c
jlchang <jlchang@broadinstitute.org>
2018-02-16 10:33:11 -0500

    adapt Miseq version to perform Hiseq MQBC

--- assessHiseqMeanQualityByCycle.sh
+++ assessHiseqMeanQualityByCycle.sh
@@ -9,6 +9,8 @@
 
 #!/usr/bin/env bash
 
+SCRIPTDIR="/cil/shed/apps/internal/chipseq/dev/v0.06"
+
 display_usage() { 
 	echo -e "\nUsage: $0 <path> <optional:output file prefix> \n" 
 	echo "expected path format: /btl/data/walkup/ChIPSeq/SSF-#/data/<flowcell>/<run folder>/<lane>"
@@ -34,9 +36,7 @@ fi
 
 #check if fastq files exist at supplied path
 lane=$(basename "$1")
-sansLane=$(dirname "$1")
-sansFolder=$(dirname $sansLane)
-flowcell=$(basename "$sansFolder")
+flowcell=$(basename $(dirname $(dirname "$1")))
 
 if [ ! -e ${dataPath}/${flowcell}.${lane}.1.fastq.gz ]
 then
@@ -47,7 +47,7 @@ fi
 #check if output prefix supplied
 if [  $# -lt 2 ]
 then
-    output_prefix=""
+    output_prefix="${flowcell}_${lane}_"
 else
     output_prefix="${2}_${flowcell}_${lane}_"
     echo "using ${output_prefix} as prefix for output files"
@@ -60,22 +60,20 @@ reuse -q .r-3.3.0
 set -o errexit
 set -o pipefail
 set -o nounset
-# set -o xtrace
+set -o xtrace
 
 
 
-java -Xmx4G -jar /seq/software/picard-public/2.14.0/picard.jar FastqToSam \
+#java -Xmx4G -jar /seq/software/picard-public/2.14.0/picard.jar FastqToSam \
 	F1=${dataPath}/${flowcell}.${lane}.1.fastq.gz \
 	F2=${dataPath}/${flowcell}.${lane}.2.fastq.gz \
 	O=unaligned.bam \
 	SM=MQBC
 
-java -Xmx4G -jar /seq/software/picard-public/2.14.0/picard.jar  MeanQualityByCycle \
+#java -Xmx4G -jar /seq/software/picard-public/2.14.0/picard.jar  MeanQualityByCycle \
 	I=unaligned.bam \
 	O=${output_prefix}unalignedBam_mean_qual_by_cycle.txt \
 	CHART=${output_prefix}unalignedBam_mean_qual_by_cycle.pdf
 
-rm R1_001.fastq.gz
-rm R2_001.fastq.gz
-rm ${output_prefix}unaligned.bam
-Rscript $SCRIPTDIR/identifyMeanQualityByCycles.R "${output_prefix}" > ${output_prefix}_below_30_qual_by_cycle_report.txt
+rm unaligned.bam
+Rscript $SCRIPTDIR/identifyMeanQualityByCycles.R "${output_prefix}" > ${output_prefix}below_30_qual_by_cycle_report.txt
