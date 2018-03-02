61d3fdcb3a47aafdd7fc90e189442832644fb6a6
jlchang <jlchang@broadinstitute.org>
2018-03-02 13:25:09 -0500

    adjust script output to be less noisy and more clear

--- collectExptMetrics.sh
+++ collectExptMetrics.sh
@@ -26,17 +26,18 @@ fi
 
 
 set -e
-echo "analysis directory is $orig"
+echo "collecting metrics from samples in $orig"
 
 
 echo "Expt	Sample	Ctrl	Tot_Reads	MAPQ1_Reads	pctAnalyzed	mapq1PE_rip	mapq1PE_FRIP	R1_CHIMERAS	R1_ADAPTER	R2_CHIMERAS	R2_ADAPTER	pDUPLICATION	EstLibSize	RSC	NSC	QT	MapRaw_Reads	PropPr_Reads	PrSing_Reads	mmdc_Reads	pDup_Reads" >> ${result}
 
 while IFS=$'\t': read sample fastq1 fastq2
 do
-  echo "collect metrics for ${orig}/${sample}_PE/${sample}.Metrics"
+#  echo "collect metrics for ${orig}/${sample}_PE/${sample}.Metrics"
   metrics=$(tail -n1 ${orig}/${sample}_PE/${sample}.Metrics)
   echo -ne "$expt\t" >> ${result}
   echo -ne "$sample\t" >> ${result}
   echo -ne "$(grep $sample control_info.txt | cut -f 2)\t" >> ${result}
   echo "$metrics" >> ${result}
 done < input_data.tsv
+
