5d75b3360ea4074998ca3914604f6af25ac73f6b
jlchang <jlchang@broadinstitute.org>
2018-03-19 13:26:32 -0400

    convert script to use PIPE_LOC

--- UGESmapAggHiSeqFastq.sh
+++ UGESmapAggHiSeqFastq.sh
@@ -5,9 +5,36 @@ reuse -q UGES
 
 set -e
 
+#check PIPE_LOC environment variable is set
+#https://stackoverflow.com/questions/307503
+: "${PIPE_LOC:?Need to set PIPE_LOC non-empty}"
+
+SCRIPTDIR="/cil/shed/apps/internal/chipseq/$PIPE_LOC"
+
+if [  ! -d "$SCRIPTDIR" ]
+then
+    echo "Unable to find $SCRIPTDIR, please check the provided PIPE_LOC value"
+    exit
+fi
+
 currDir=$(pwd)
 echo "current Directory is $currDir"
 fastq="$currDir/fastq"
+
+if [ -d $currDir/fastq ]
+then
+    echo "output dir exists:"
+    echo "rmdir $currDir/fastq"
+  exit
+fi
+
+if [ ! -d seq_prep ]
+then
+    echo "missing seq_prep dir, please check that all lanes contain the same samples before proceeding"
+    exit
+fi
+
+
 echo "making $fastq"
 mkdir $fastq
 
@@ -16,6 +43,6 @@ echo "obtain data from $path"
 
 while IFS=$'\t': read barcode1 barcode2 sampleName
 do
-  echo "qsub -q btl /cil/shed/apps/internal/chipseq/prod/aggregateFastqs.sh $fastq $path $barcode1 $barcode2 $sampleName"
-  qsub -q btl /cil/shed/apps/internal/chipseq/prod/aggregateFastqs.sh $fastq $path $barcode1 $barcode2 $sampleName
+  echo "qsub -q btl -cwd -o seq_prep -e seq_prep $SCRIPTDIR/aggregateFastqs.sh $fastq $path $barcode1 $barcode2 $sampleName"
+  qsub -q btl -cwd -o seq_prep -e seq_prep $SCRIPTDIR/aggregateFastqs.sh $fastq $path $barcode1 $barcode2 $sampleName
 done  < barcode_mapping.txt
